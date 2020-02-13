8f34a510f8157adffa896e7fafd3c64b5846efd9
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-02-13 14:46:26 -0800

    created server

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    const songs =[
+        {
+            id: 1,
+            title: "Call me baby"
+        },
+        {
+            id: 2,
+            title: "Love me right"
+        },
+        {
+            id: 3,
+            title: "Lucky One"
+        },
+        {
+            id: 4,
+            title: "Unfair"
+        },
+        {
+            id: 5,
+            title: "Overdose"
+        },
+    ];
+
+    res.status(200).json(songs);
+})
+
+
+const port = 4000;
+server.listen(port, () => 
+    console.log(`Running magic on ${port}`));
